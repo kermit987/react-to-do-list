508e51f53ca9df5789d98282752dcf37f6c250e5
steven <steven.loofat@yahoo.fr>
2020-05-25 18:47:21 +0200

    [+] components/DisplayTask feature to delete task done

--- src/components/DisplayTask.jsx
+++ src/components/DisplayTask.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default class DisplayTak extends React.Component {
   render() {
     const listTask = this.props.tasks.map((task, index) => (
-      <li key={index}>{task}</li>
+      <li key={index} onClick={() => this.props.deleteTask(task)}>
+        {task}
+      </li>
     ))
 
     return (
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -15,9 +15,25 @@ export default class Input extends React.Component {
     this.setState({ currentTyping: e.target.value })
   }
 
-  handleClick = (e) => {
-    const tmp = this.state.tasks.concat(this.state.currentTyping)
-    this.setState({ tasks: tmp })
+  handleSubmit = (e) => {
+    const updatedTasks = this.state.tasks.concat(this.state.currentTyping)
+
+    this.setState({ tasks: updatedTasks })
+    this.setState({ currentTyping: '' })
+  }
+
+  handleDelete = (description) => {
+    let tasks = this.state.tasks
+
+    tasks.forEach((task, index) => {
+      if (task === description) {
+        const left = tasks.slice(0, index)
+        const right = tasks.slice(index + 1)
+
+        const updatedTasks = left.concat(right)
+        this.setState({ tasks: updatedTasks })
+      }
+    })
   }
 
   render() {
@@ -25,8 +41,8 @@ export default class Input extends React.Component {
       <fieldset>
         <legend>Enter a task to complete!</legend>
         <input value={this.state.currentTyping} onChange={this.handleChange} />
-        {<button onClick={this.handleClick}>Add new task</button>}
-        <DisplayTask tasks={this.state.tasks} />
+        {<button onClick={this.handleSubmit}>Add new task</button>}
+        <DisplayTask tasks={this.state.tasks} deleteTask={this.handleDelete} />
       </fieldset>
     )
   }
